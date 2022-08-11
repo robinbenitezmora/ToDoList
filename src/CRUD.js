f574e6fa01b848844d418827fce67ec08f854440
robinbenitezmora <robinbenitez1990@gmail.com>
2022-08-11 15:59:08 -0500

    Modify the CRUD.js file in order to work better

--- src/CRUD.js
+++ src/CRUD.js
@@ -1,8 +1,6 @@
 import { storage } from "./storage.js";
 import { clearAllFields } from "./clean.js";
 
-export { addActivity, removeActivity, activities, updateStorage };
-
 const list = document.getElementById("to-do-list");
 const removeBtn = document.getElementById('clearBtn');
 
@@ -14,7 +12,7 @@ class Activities {
   }
 };
 
-const activities = [];
+let activities = [];
 
 const addActivity = (activityValue) => {
   const localData = JSON.parse(localStorage.getItem('task'));
@@ -40,82 +38,82 @@ const addActivity = (activityValue) => {
     });
   });
 
-const newActivity = new Activities(activityValue, false, checkbox.length);
-activities.push(newActivity);
-storage(activities);
+  const newActivity = new Activities(activityValue, false, checkbox.length);
+  activities.push(newActivity);
+  storage(activities);
 
-const edit = document.querySelectorAll('.fa-ellipsis-v');
-edit.forEach((item) => {
-  item.addEventListener('click', () => {
-    editActivity(activitiyContainer, item.previousElementSibling);
-    item.parentElement.classList.add('selected-item');
-  });
-});
-
-const remove = document.querySelectorAll('.fa-trash');
-remove.forEach((item) => {
-  item.addEventListener('click', () => {
-    removeActivity(item.parentElement);
-  });
-});
-};
-
-const removeActivity = (activity) => {
-  list.removeChild(activity);
-  let count = 0;
-  const localData = JSON.parse(localStorage.getItem('task'));
-  const data = Array.from(localData).filter((item) => item.completed === false);
-  data.map((item) => item.index = count += 1);
-  storage(data);
-};
-const editActivity = (activitiyContainer, activity) => {
-  const editText = document.createElement('input');
-  editText.type = 'text';
-  editText.className = 'edit-text';
-  editText.value = activity.textContent;
-  activitiyContainer.replaceChild(editText, activity);
-  editText.addEventListener('keypress', (item) => {
-    if (item.key === 'Enter') {
-      const activityContainers = document.querySelectorAll('.list-item');
-      const localData = JSON.parse(localStorage.getItem('task'));
-      for (let i = 0; i < activityContainers.length; i += 1) {
-        if (activityContainers[i].classList.contains('selected-item')) {
-          localData[i].description = editText.value;
-          storage(localData);
-        }
-      }
-      editText.parentElement.classList.remove('selected-item');
-      activitiyContainer.replaceChild(activity, editText);
-      activity.textContent = editText.value;
-    }
-  });
-};
-
-const getStorage = () => {
-  const data = JSON.parse(localStorage.getItem('task'));
-  if (!data) {
-    localStorage.setItem('task', JSON.stringify([]));
-  }
-
-  data.map((item) => {
-    const ativitiyContainer = document.createElement('li');
-    ativitiyContainer.className = 'list-item';
-    ativitiyContainer.innerHTML += `
-      <input type= 'checkbox' class='checkbox'>
-      <span class='description'>${item.description}</span>
-      <i class='fas fa-ellipsis-v'></i>
-      <i class='fas fa-trash'></i>
-    `;
-    list.appendChild(ativitiyContainer);
-
-    const edit = document.querySelectorAll('.fa-ellipsis-v');
-    edit.forEach((item) => {
-      item.addEventListener('click', () => {
-        editActivity(ativitiyContainer, item.previousElementSibling);
-        item.parentElement.classList.add('selected-item');
-      });
+  const edit = document.querySelectorAll('.fa-ellipsis-v');
+  edit.forEach((item) => {
+    item.addEventListener('click', () => {
+      editActivity(activitiyContainer, item.previousElementSibling);
+      item.parentElement.classList.add('selected-item');
     });
   });
+
+  const remove = document.querySelectorAll('.fa-trash');
+  remove.forEach((item) => {
+    item.addEventListener('click', () => {
+      removeActivity(item.parentElement);
+    });
+  });
+};
+
+  const removeActivity = (activity) => {
+    list.removeChild(activity);
+    let count = 0;
+    const localData = JSON.parse(localStorage.getItem('task'));
+    const data = Array.from(localData).filter((item) => item.completed === false);
+    data.map((item) => item.index = count += 1);
+    storage(data);
+  };
+  const editActivity = (activitiyContainer, activity) => {
+    const editText = document.createElement('input');
+    editText.type = 'text';
+    editText.className = 'edit-text';
+    editText.value = activity.textContent;
+    activitiyContainer.replaceChild(editText, activity);
+    editText.addEventListener('keypress', (item) => {
+      if (item.key === 'Enter') {
+        const activityContainers = document.querySelectorAll('.list-item');
+        const localData = JSON.parse(localStorage.getItem('task'));
+        for (let i = 0; i < activityContainers.length; i += 1) {
+          if (activityContainers[i].classList.contains('selected-item')) {
+            localData[i].description = editText.value;
+            storage(localData);
+          }
+        }
+        editText.parentElement.classList.remove('selected-item');
+        activitiyContainer.replaceChild(activity, editText);
+        activity.textContent = editText.value;
+      }
+    });
+  };
+
+  const getStorage = () => {
+    const data = JSON.parse(localStorage.getItem('task'));
+    if (!data) {
+      localStorage.setItem('task', JSON.stringify([]));
+    }
+
+    data.map((item) => {
+      const ativitiyContainer = document.createElement('li');
+      ativitiyContainer.className = 'list-item';
+      ativitiyContainer.innerHTML += `
+        <input type= 'checkbox' class='checkbox'>
+        <span class='description'>${item.description}</span>
+        <i class='fas fa-ellipsis-v'></i>
+        <i class='fas fa-trash'></i>
+      `;
+      list.appendChild(ativitiyContainer);
+
+      const edit = document.querySelectorAll('.fa-ellipsis-v');
+      edit.forEach((item) => {
+        item.addEventListener('click', () => {
+          editActivity(ativitiyContainer, item.previousElementSibling);
+          item.parentElement.classList.add('selected-item');
+        });
+      });
+    });
 
   const checkbox = document.querySelectorAll('.checkbox');
   checkbox.forEach((item) => {
@@ -156,3 +154,5 @@ const updateStorage = () => {
 removeBtn.addEventListener('click', () => {
   clearAllFields();
 });
+
+export { addActivity, removeActivity, activities, updateStorage };
